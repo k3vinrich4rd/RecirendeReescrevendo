ac99c429e9c2266e7a7b1c550fc1c2b1a6f76f73
k3vinrich4rd <kevinrichardve@gmail.com>
2022-09-30 12:15:51 -0300

    Inserindo escopo do projeto

---
+++ src/main/java/com/example/miniProjetoRecirende/MiniProjetoRecirendeApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniProjetoRecirende;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjetoRecirendeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjetoRecirendeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miniProjetoRecirende/MiniProjetoRecirendeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniProjetoRecirende;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjetoRecirendeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
