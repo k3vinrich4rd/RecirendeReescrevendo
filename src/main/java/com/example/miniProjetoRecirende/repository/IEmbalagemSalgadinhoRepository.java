dbe1f4c89e68de7aa0ed6037d4f253853a6ed95e
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-02 22:37:37 -0300

    Inserindo criação da interface 'IEmbalagemSalgadinhoRepository'

--- src/main/java/com/example/miniProjetoRecirende/repository/IEmbalagemSalgadinhoRepository.java
+++ src/main/java/com/example/miniProjetoRecirende/repository/IEmbalagemSalgadinhoRepository.java
@@ -1,2 +1,9 @@
-package com.example.miniProjetoRecirende.repository;public interface IEmbalagemSalgadinhoRepository {
+package com.example.miniProjetoRecirende.repository;
+
+import com.example.miniProjetoRecirende.model.EmbalagemSalgadinhoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IEmbalagemSalgadinhoRepository extends JpaRepository<EmbalagemSalgadinhoModel, Long> {
 }
