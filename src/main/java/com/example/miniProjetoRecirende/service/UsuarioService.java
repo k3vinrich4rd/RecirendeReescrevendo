f0417934651d37986a448a502d75d68def978286
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-02 22:10:41 -0300

    Inserindo implementações lógicas feitas na 'UsuarioService'

--- src/main/java/com/example/miniProjetoRecirende/service/UsuarioService.java
+++ src/main/java/com/example/miniProjetoRecirende/service/UsuarioService.java
@@ -1,9 +1,13 @@
 package com.example.miniProjetoRecirende.service;
 
+import com.example.miniProjetoRecirende.exception.EntityNotFoundException;
 import com.example.miniProjetoRecirende.model.UsuarioModel;
 import com.example.miniProjetoRecirende.repository.IUsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -14,4 +18,38 @@ public class UsuarioService {
     public UsuarioModel cadastrarUsuarios(UsuarioModel usuarioModel) {
         return iUsuarioRepository.save(usuarioModel);
     }
+
+
+    //Validação(Query method)
+    public boolean existsByCpfUsuario(String cpfUsuario) {
+        return iUsuarioRepository.existsByCpfDoUsuario(cpfUsuario);
+    }
+
+    public boolean existsByEmailDoUsuario(String existsByEmailDoUsuario) {
+        return iUsuarioRepository.existsByEmailDoUsuario(existsByEmailDoUsuario);
+    }
+
+    public boolean existsByTelefoneDoUsuario(String existsByTelefoneDoUsuario) {
+        return iUsuarioRepository.existsByTelefoneDoUsuario(existsByTelefoneDoUsuario);
+    }
+
+    public List<UsuarioModel> exibirUsuarios() {
+        return iUsuarioRepository.findAll();
+    }
+
+    public Optional<UsuarioModel> exibirUsuarioViaId(Long id) {
+        return Optional.ofNullable(iUsuarioRepository.findById(id).orElseThrow((() -> new EntityNotFoundException("Erro: id não encontrado, impossivel efetuar busca pelo id " + id))));
+    }
+
+    public UsuarioModel alterarUsuarioCadastrado(UsuarioModel usuarioModel, Long id) {
+        iUsuarioRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Erro: id não encontrado, impossivel efetuar uma alteração " + id));
+        return iUsuarioRepository.save(usuarioModel);
+    }
+
+
+    public void deletarUsuario(Long id) {
+        iUsuarioRepository.deleteById(id);
+    }
+
+
 }
