420a1d14a1908decf476a3def77cdf83f17c3150
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-02 22:06:05 -0300

    Inserindo implementações e criação de 'ExceptionHandlerRecirende'

--- src/main/java/com/example/miniProjetoRecirende/exception/ExceptionHandlerRecirende.java
+++ src/main/java/com/example/miniProjetoRecirende/exception/ExceptionHandlerRecirende.java
@@ -12,12 +12,14 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionHandlerRecirende {
-
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> exceptionHandlerEntradaInvalida(HttpMessageNotReadableException ex) {
         return new ResponseEntity<>("Campo inválido.", HttpStatus.UNPROCESSABLE_ENTITY);
@@ -44,6 +46,19 @@ public class ExceptionHandlerRecirende {
         });
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
+
+    //Método de tratamento de mensagens not found
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<PadraoErro> entityNotFound(EntityNotFoundException e, HttpServletRequest servletRequest) {
+        PadraoErro erro = new PadraoErro();
+        erro.setTimestamp(Instant.now());
+        erro.setStatus(HttpStatus.NOT_FOUND.value());
+        erro.setError("Resource Not Found");
+        erro.setMessage(e.getMessage());
+        erro.setPath(servletRequest.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
 
 }
 
