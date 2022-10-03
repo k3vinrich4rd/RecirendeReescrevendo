e643a9405eb60ee094088c9cd79c3ed04695b1a3
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-02 22:38:19 -0300

    Inserindo criação da classe 'EmbalagemSalgadinhoService'

--- src/main/java/com/example/miniProjetoRecirende/service/EmbalagemSalgadinhoService.java
+++ src/main/java/com/example/miniProjetoRecirende/service/EmbalagemSalgadinhoService.java
@@ -1,2 +1,12 @@
-package com.example.miniProjetoRecirende.service;public class EmbalagemSalgadinhoService {
+package com.example.miniProjetoRecirende.service;
+
+import com.example.miniProjetoRecirende.repository.IEmbalagemSalgadinhoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmbalagemSalgadinhoService {
+
+    @Autowired
+    private IEmbalagemSalgadinhoRepository iEmbalagemSalgadinhoRepository;
 }
