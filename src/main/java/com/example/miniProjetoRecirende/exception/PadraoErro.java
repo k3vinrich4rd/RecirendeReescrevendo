5c6ffc57384fa3c9c97b17ca45ada90d10b93083
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-02 22:06:54 -0300

    Inserindo criação da classe 'PadraoErro'

--- src/main/java/com/example/miniProjetoRecirende/exception/PadraoErro.java
+++ src/main/java/com/example/miniProjetoRecirende/exception/PadraoErro.java
@@ -1,9 +1,17 @@
 package com.example.miniProjetoRecirende.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.time.Instant;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PadraoErro implements Serializable {
+    private static final Long serialVersionUID = 1L;
 
     private Instant timestamp;
     private Integer status;
